e54480a2f08d4f29d16b998182c3d46edf84e5ee
tonymitappinventor <tonymitappinventor@gmail.com>
2026-02-04 16:45:54 -0300

    feat: update Prisma schema and client configuration
    - Updated `schema.prisma` to set the output directory for the Prisma client.
    - Modified `prisma.ts` to import the Prisma client from the generated directory and added PostgreSQL adapter configuration.
    - Added `.vscode/settings.json` to disable Postman dotenv detection notifications.
    - Generated new files for Prisma client, including `client.ts`, `browser.ts`, `commonInputTypes.ts`, `enums.ts`, and internal files for class and namespace handling.
    - Enhanced internal type definitions and utility types for better type safety and usability.

--- src/infrastructure/database/prisma.ts
+++ src/infrastructure/database/prisma.ts
@@ -1,13 +1,16 @@
 // src/infrastructure/database/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../generated/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
 // Prevent multiple instances of Prisma Client in development
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query", "error", "warn"], // Useful for debugging your "feature/postgres-integration"
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
